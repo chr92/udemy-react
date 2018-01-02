77351105c3c632e253d958de7372f4798899a7c0
Christiaan Hendriksen <christiaan.hendriksen@hotjar.com>
2018-01-02 12:42:44 +0000

    Lecture 51

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('app.js is running!');
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(100,23));
+
+// person.js
+// named export isAdult(18) - true or false
+// named export canDrink(18) - true or false
+
+import { isAdult, canDrink } from './person.js'
+
+console.log(isAdult(18));
+console.log(canDrink(18));
